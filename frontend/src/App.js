8f2d4b1c8ff654669441f7709140cc987ab36588
ColeTrammer <cole.trammer@gmail.com>
2020-11-06 18:12:56 -0800

    Frontend: add simple Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,22 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./Navbar";
 
 export default function App() {
     return (
         <Router>
-            <div>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/about">About</Link>
-                        </li>
-                        <li>
-                            <Link to="/users">Users</Link>
-                        </li>
-                    </ul>
-                </nav>
-                <Switch>
-                    <Route path="/about">
-                        <About />
-                    </Route>
-                    <Route path="/users">
-                        <Users />
-                    </Route>
-                    <Route path="/">
-                        <Home />
-                    </Route>
-                </Switch>
-            </div>
+            <Navbar></Navbar>
+            <Switch>
+                <Route path="/about">
+                    <About />
+                </Route>
+                <Route path="/users">
+                    <Users />
+                </Route>
+                <Route path="/">
+                    <Home />
+                </Route>
+            </Switch>
         </Router>
     );
 }
---
+++ frontend/src/Navbar.js
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
