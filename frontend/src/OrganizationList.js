3a91163d8bd4abfd9302eb1a5e24ed7bf2c8462d
ColeTrammer <cole.trammer@gmail.com>
2020-11-06 19:37:08 -0800

    Add mock up for orgs page

--- frontend/src/OrganizationList.js
+++ frontend/src/OrganizationList.js
@@ -1,5 +1,23 @@
-function OrganizationList() {
-    return <h1>Organizations</h1>;
+import { Card, CardContent, Typography } from "@material-ui/core";
+import React from "react";
+
+const FAKE_DATA = [
+    { name: "Team Liquid" },
+    { name: "Team Solo Mid" },
+    { name: "Couter Logic Gamer" },
+    { name: "G2" },
+];
+
+class OrganizationList extends React.Component {
+    render = () => {
+        return FAKE_DATA.map((org) => (
+            <Card>
+                <CardContent>
+                    <Typography variant="h4">{org.name}</Typography>
+                </CardContent>
+            </Card>
+        ));
+    };
 }
 
 export default OrganizationList;
