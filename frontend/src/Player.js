f9106b4950409c0925ec0362eec660cc8d68f29b
Alex Yu <yu.alexanderx@gmail.com>
2020-11-06 23:26:35 -0800

    Add constant for API query limit

--- frontend/src/Player.js
+++ frontend/src/Player.js
@@ -10,7 +10,7 @@ class Player extends React.Component {
         this.state = {
             info: null
         };
-        
+
         this.getData = this.getData.bind(this);
         this.filter = this.filter.bind(this);
     }
@@ -20,7 +20,7 @@ class Player extends React.Component {
         let data = new FormData();
         data.append("wiki", Constants.GAMES.join("|"));
         data.append("apikey", Constants.LIQUID_API_KEY);
-        data.append("limit", 5000);
+        data.append("limit", Constants.MAXIMUM_QUERY_LIMIT);
 
         this.getData(true, data, 0);
 
@@ -29,7 +29,7 @@ class Player extends React.Component {
     getData = (shouldContinue, data, offset) => {
 
         if(shouldContinue) {
-            data.set("offset", offset + 5000);
+            data.set("offset", offset + Constants.MAXIMUM_QUERY_LIMIT);
 
             fetch(`${Constants.LIQUID_API_URL}${Constants.TRANSFER_LIST_ENDPOINT}`, {
                 method: "POST",
@@ -41,7 +41,7 @@ class Player extends React.Component {
             })
                 .then((response) => response.json())
                 .then((data) => this.setState({ info: data.result }))
-                .then(() => this.getData(this.state.info.length >= 5000, data, offset + 5000))
+                .then(() => this.getData(this.state.info.length >= Constants.MAXIMUM_QUERY_LIMIT, data, offset + Constants.MAXIMUM_QUERY_LIMIT))
                 .catch((err) => console.log(err));
         }
 
