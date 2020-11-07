491e3de03595fd564f8caa6d4052c487d3a16ed9
ColeTrammer <cole.trammer@gmail.com>
2020-11-06 18:57:40 -0800

    Start working on the HomePage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,20 @@
 import React from "react";
+import { Container } from "@material-ui/core";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./Navbar";
+import HomePage from "./HomePage";
 
 export default function App() {
     return (
         <Router>
             <Navbar></Navbar>
-            <Switch>
-                <Route path="/about">
-                    <About />
-                </Route>
-                <Route path="/users">
-                    <Users />
-                </Route>
-                <Route path="/">
-                    <Home />
-                </Route>
-            </Switch>
+            <Container>
+                <Switch>
+                    <Route path="/">
+                        <HomePage />
+                    </Route>
+                </Switch>
+            </Container>
         </Router>
     );
-}
-
-function Home() {
-    return <h2>Home</h2>;
-}
-
-function About() {
-    return <h2>About</h2>;
-}
-
-function Users() {
-    return <h2>Users</h2>;
 }
