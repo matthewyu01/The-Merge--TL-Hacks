2128207eaf5fc787a73e40dcca6d64df2688a815
Alex Yu <yu.alexanderx@gmail.com>
2020-11-06 18:54:21 -0800

    Add CORS functionality

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,24 @@
 from flask import Flask, jsonify
+from flask_cors import CORS, cross_origin
+
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def hello():
     return "Hello World!"
 
-@app.route('/players/<name>/')
+@app.route('/players/<name>/', methods=['GET'])
 def get_player_info(name):
-    return jsonify(
-        name=name
-    )
+    return jsonify({
+        name: name
+    })
+
+@app.route('/teams/<name>/', methods=['GET'])
+def get_team_info(name):
+    return jsonify({
+        name: name
+    })
 
 if __name__ == "__main__":
     app.run()
