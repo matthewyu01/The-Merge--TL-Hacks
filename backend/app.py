490705b983eb0e8f3cb670c9b93d71ef596966eb
Alex Yu <yu.alexanderx@gmail.com>
2020-11-06 18:29:00 -0800

    Create endpoint to get player info

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return "Hello World!"
+
+@app.route('/players/<name>/')
+def get_player_info(name):
+    return jsonify(
+        name=name
+    )
 
 if __name__ == "__main__":
     app.run()
