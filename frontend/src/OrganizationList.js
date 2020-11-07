d90181a188847136bc82ca8e95ff70b7b3cd83f8
Alex Yu <yu.alexanderx@gmail.com>
2020-11-07 00:35:07 -0800

    Add filter for organizations by games

--- frontend/src/OrganizationList.js
+++ frontend/src/OrganizationList.js
@@ -1,7 +1,10 @@
 import {
+    Button,
     Card,
     CardContent,
     Grid,
+    Menu,
+    MenuItem,
     Typography,
     CardActionArea,
 } from "@material-ui/core";
@@ -22,7 +25,14 @@ class OrganizationList extends React.Component {
 
         this.state = {
             orgs: {},
+            filtered: {},
+            menuOpen: null,
+            selectedIndex: 0,
         };
+
+        this.handleClick = this.handleClick.bind(this);
+        this.handleMenuItemClick = this.handleMenuItemClick.bind(this);
+        this.handleClose = this.handleClose.bind(this);
     }
 
     componentDidMount() {
@@ -65,8 +75,51 @@ class OrganizationList extends React.Component {
                         return oldState;
                     });
                 })
+                .then(() => this.setState({
+                    filtered: this.state.orgs
+                }))
                 .catch((err) => console.log(err));
         });
+    }
+
+    handleClick = (event) => {
+        this.setState({
+            menuOpen: event.currentTarget
+        });
+    }
+
+    handleMenuItemClick = (event, index) => {
+        if(index === 0) {
+            this.setState({
+                filtered: this.state.orgs,
+                selectedIndex: 0,
+                menuOpen: null
+            });
+        } else {
+            let newFiltered = {};
+            let toFilter = Constants.GAMES[index-1];
+
+            for (var key of Object.keys(this.state.orgs)) {
+                if(this.state.orgs[key].games.includes(toFilter)) {
+                    newFiltered[key] = this.state.orgs[key];
+                }
+            }
+
+            this.setState({
+                filtered: newFiltered,
+                selectedIndex: index,
+                menuOpen: null
+            })
+
+            console.log(newFiltered);
+        }
+        
+    };
+
+    handleClose = () => {
+        this.setState({
+            menuOpen: null
+        })
     }
 
     render = () => {
@@ -74,30 +127,56 @@ class OrganizationList extends React.Component {
 
         const { classes } = this.props;
         return (
-            <Grid container spacing={2} className={classes.root}>
-                {Object.keys(this.state.orgs).map((key, i) => {
-                    const org = this.state.orgs[key];
-                    return (
-                        <Grid item xs={6} key={`orgs-${org.name}-${i}`}>
-                            <Card variant="outlined">
-                                <Link
-                                    to={`/organizations/${org.name}`}
-                                    component={CardActionArea}
-                                >
-                                    <CardContent>
-                                        <Typography variant="h6">
-                                            {org.name}
-                                        </Typography>
-                                        <Typography variant="body2">
-                                            {org.games.join(", ")}
-                                        </Typography>
-                                    </CardContent>
-                                </Link>
-                            </Card>
-                        </Grid>
-                    );
-                })}
-            </Grid>
+            <div>
+                <Button aria-controls="simple-menu" aria-haspopup="true" onClick={this.handleClick}>
+                    Filter by Game
+                </Button>
+                <Menu
+                    anchorEl={this.state.menuOpen}
+                    keepMounted
+                    open={Boolean(this.state.menuOpen)}
+                    onClose={this.handleClose}
+                >
+                    <MenuItem 
+                        key={0} 
+                        onClick={(event) => this.handleMenuItemClick(event, 0)} 
+                        selected={this.state.selectedIndex === 0}>
+                        All Games
+                    </MenuItem>
+                    {Constants.GAMES.map((game, i) => {
+                        return <MenuItem 
+                                    key={i+1}
+                                    onClick={(event) => this.handleMenuItemClick(event, i+1)} 
+                                    selected={this.state.selectedIndex === i+1}>
+                                    {game}
+                                </MenuItem>;
+                    })}
+                </Menu>
+                <Grid container spacing={2} className={classes.root}>
+                    {Object.keys(this.state.filtered).map((key, i) => {
+                        const org = this.state.filtered[key];
+                        return (
+                            <Grid item xs={6} key={`orgs-${org.name}-${i}`}>
+                                <Card variant="outlined">
+                                    <Link
+                                        to={`/organizations/${org.name}`}
+                                        component={CardActionArea}
+                                    >
+                                        <CardContent>
+                                            <Typography variant="h6">
+                                                {org.name}
+                                            </Typography>
+                                            <Typography variant="body2">
+                                                {org.games.join(", ")}
+                                            </Typography>
+                                        </CardContent>
+                                    </Link>
+                                </Card>
+                            </Grid>
+                        );
+                    })}
+                </Grid>
+            </div>
         );
     };
 }
