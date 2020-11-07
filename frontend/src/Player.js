4956263b3b4739ad1b0f16efad15ead3fa448065
ColeTrammer <cole.trammer@gmail.com>
2020-11-07 11:38:50 -0800

    Hard Code Player Name into transfer

--- frontend/src/Player.js
+++ frontend/src/Player.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import Chart from 'react-google-charts'; 
-import { Typography } from '@material-ui/core';
-import * as Constants from './Constants';
+import React from "react";
+import Chart from "react-google-charts";
+import { Typography } from "@material-ui/core";
+import * as Constants from "./Constants";
 
 class Player extends React.Component {
-
     constructor(props) {
         super(props);
 
         this.state = {
-            info: null
+            info: null,
         };
 
         this.getData = this.getData.bind(this);
@@ -17,49 +16,57 @@ class Player extends React.Component {
     }
 
     componentDidMount() {
-
         let data = new FormData();
-        data.append("wiki", Constants.GAMES.join("|"));
+        data.append("wiki", "dota2");
         data.append("apikey", Constants.LIQUID_API_KEY);
         data.append("limit", Constants.MAXIMUM_QUERY_LIMIT);
 
         this.getData(true, data, 0);
-
     }
 
     getData = (shouldContinue, data, offset) => {
+        if (shouldContinue) {
+            data.set("offset", offset);
+            data.set("conditions", `[[player::ELeVeN]]`);
 
-        if(shouldContinue) {
-            data.set("offset", offset + Constants.MAXIMUM_QUERY_LIMIT);
-
-            fetch(`${Constants.LIQUID_API_URL}${Constants.TRANSFER_LIST_ENDPOINT}`, {
-                method: "POST",
-                mode: "cors",
-                headers: {
-                    "Content-Type": "application/x-www-form-urlencoded",
-                },
-                body: new URLSearchParams(data),
-            })
+            fetch(
+                `${Constants.LIQUID_API_URL}${Constants.TRANSFER_LIST_ENDPOINT}`,
+                {
+                    method: "POST",
+                    mode: "cors",
+                    headers: {
+                        "Content-Type": "application/x-www-form-urlencoded",
+                    },
+                    body: new URLSearchParams(data),
+                }
+            )
                 .then((response) => response.json())
-                .then((data) => this.setState({ info: data.result }))
-                .then(() => this.getData(this.state.info.length >= Constants.MAXIMUM_QUERY_LIMIT, data, offset + Constants.MAXIMUM_QUERY_LIMIT))
+                .then((data) => {
+                    console.log(data);
+                    this.setState({ info: data.result });
+                })
+                .then(() =>
+                    this.getData(
+                        this.state.info.length >= Constants.MAXIMUM_QUERY_LIMIT,
+                        data,
+                        offset + Constants.MAXIMUM_QUERY_LIMIT
+                    )
+                )
                 .catch((err) => console.log(err));
         }
-
-    }
+    };
 
     filter = () => {
-
         return this.state.info.filter((player) => {
-            return player.extradata.displayname.toLowerCase() === this.props.match.params.player.toLowerCase();
+            return (
+                player.extradata.displayname.toLowerCase() ===
+                this.props.match.params.player.toLowerCase()
+            );
         });
-
-    }
+    };
 
     render = () => {
-
-        if(!this.state.info)
-            return null;
+        if (!this.state.info) return null;
 
         let playerInfo = this.filter();
         playerInfo.sort((a, b) => {
@@ -67,39 +74,40 @@ class Player extends React.Component {
         });
 
         let timelineData = [];
-        timelineData.push([{ type: 'string', id: 'Team No.' },
-                            { type: 'string', id: 'Team Name' },
-                            { type: 'date', id: 'Start' },
-                            { type: 'date', id: 'End' }]);
+        timelineData.push([
+            { type: "string", id: "Team No." },
+            { type: "string", id: "Team Name" },
+            { type: "date", id: "Start" },
+            { type: "date", id: "End" },
+        ]);
 
         let i = playerInfo.length;
         let currDate = Date.now();
-        for(var entry of playerInfo) {
-
-            timelineData.push([`Team #${i.toString()}`, entry.toteam, new Date(Date.parse(entry.date)), currDate]);
+        for (var entry of playerInfo) {
+            timelineData.push([
+                `Team #${i.toString()}`,
+                entry.toteam,
+                new Date(Date.parse(entry.date)),
+                currDate,
+            ]);
             currDate = new Date(Date.parse(entry.date));
             i--;
-
         }
 
         if (timelineData.length === 1) {
-            return (<Typography variant="h6">No Data Available</Typography>)
+            return <Typography variant="h6">No Data Available</Typography>;
         }
 
         return (
-
             <Chart
-                width={'500px'}
-                height={'300px'}
+                width={"500px"}
+                height={"300px"}
                 chartType="Timeline"
                 loader={<div>Loading Chart</div>}
                 data={timelineData}
             />
-
         );
-
-    }
-
+    };
 }
 
 export default Player;
