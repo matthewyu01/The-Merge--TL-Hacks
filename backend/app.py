e813909038b9eba0c653bd49b2a2d92be57d0dda
matthewyu01 <yumatthew10@gmail.com>
2020-11-07 15:29:49 -0800

    Add route for images

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, send_from_directory
 from flask_cors import CORS, cross_origin
 
 
@@ -8,6 +8,10 @@ CORS(app)
 @app.route('/')
 def hello():
     return "Hello World!"
+
+@app.route('/images/<path:path>')
+def get_image(path):
+    return send_from_directory('images', path)
 
 
 if __name__ == "__main__":
