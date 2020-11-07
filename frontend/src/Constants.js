de904954b6f14b2b21c2ac550fd554e9a1470c4b
matthewyu01 <yumatthew10@gmail.com>
2020-11-07 11:55:12 -0800

    Add Steampowered API constants

--- frontend/src/Constants.js
+++ frontend/src/Constants.js
@@ -1,5 +1,6 @@
 export const BACKEND_URL = "http://127.0.0.1:5000/";
 export const LIQUID_API_URL = "https://api.liquipedia.net/api/";
+export const STEAMPOWERED_API_URL = "https://api.steampowered.com/";
 
 export const ORG_LIST_ENDPOINT = "orgs/";
 export const PLAYER_LIST_ENDPOINT = "v1/player";
@@ -7,9 +8,11 @@ export const TEAM_LIST_ENDPOINT = "v1/team";
 export const TRANSFER_LIST_ENDPOINT = "v1/transfer";
 
 export const MAXIMUM_QUERY_LIMIT = 5000;
-
 export const ORGS_PER_PAGE = 20;
 export const PLAYERS_PER_PAGE = 20;
+
+export const STEAM_PLAYERCOUNT_ENDPOINT = "ISteamUserStats/GetNumberOfCurrentPlayers/v1/"
+
 export const GAMES = ["dota2", "counterstrike", "leagueoflegends", "valorant"];
 export const GAMES_PRETTY = {
     dota2: "Dota 2",
@@ -17,5 +20,12 @@ export const GAMES_PRETTY = {
     leagueoflegends: "League of Legends",
     valorant: "Valorant",
 };
+
+export const STEAM_GAMES = ["dota2", "counterstrike"]
+export const STEAM_GAME_IDS = {
+    dota2: "570",
+    counterstrike: "730",
+};
+
 export const LIQUID_API_KEY =
     "E9s8G9svmgX1uqPqnC7MxJzGeThepfres9q2VLWBmSreZ6XEAxRFWLimUfVlUEzCjZiizMyFQTBRR652OPBo4MOFk9N2M9MzujsANC63acuvCIPHvY4zIO4279Fneu3p";
