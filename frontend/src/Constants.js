a6245908c751376c6c1278b0db23f3be846f3251
Alex Yu <yu.alexanderx@gmail.com>
2020-11-06 23:23:55 -0800

    Add constant for API query limit

--- frontend/src/Constants.js
+++ frontend/src/Constants.js
@@ -6,6 +6,8 @@ export const PLAYER_LIST_ENDPOINT = "players/";
 export const TEAM_LIST_ENDPOINT = "v1/team";
 export const TRANSFER_LIST_ENDPOINT = "v1/transfer";
 
+export const MAXIMUM_QUERY_LIMIT = 5000;
+
 export const GAMES = ["dota2", "counterstrike", "leagueoflegends", "valorant"];
 export const LIQUID_API_KEY =
     "E9s8G9svmgX1uqPqnC7MxJzGeThepfres9q2VLWBmSreZ6XEAxRFWLimUfVlUEzCjZiizMyFQTBRR652OPBo4MOFk9N2M9MzujsANC63acuvCIPHvY4zIO4279Fneu3p";
